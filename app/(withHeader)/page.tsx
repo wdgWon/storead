b061db877c9a3cee9618ce668b6a045c1f688341
genTe <128919388+wdgWon@users.noreply.github.com>
2024-04-29 23:05:37 +0900

    [Feat ✏️] 기존 루트 페이지에서 루트 라우트에 렌더링되던 헤더를 헤더 레이아웃 페이지에 통합하여 단일화

--- app/(withHeader)/page.tsx
+++ app/(withHeader)/page.tsx
@@ -1,5 +1,10 @@
-function WithHeaderPage() {
-  return null;
-}
+import DefaultLayout from "@/components/default-layout/default-layout";
+import MainLayout from "@/components/main-layout/main-layout";
 
-export default WithHeaderPage;
+export default function WithHeaderPage() {
+  return (
+    <DefaultLayout>
+      <MainLayout />
+    </DefaultLayout>
+  );
+}

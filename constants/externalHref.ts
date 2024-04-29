8b93cde49731c58e26610ad0fc307dbeb316c243
genTe <128919388+wdgWon@users.noreply.github.com>
2024-04-29 23:10:50 +0900

    [Feat ✏️] 엔드포인트, 외부 링크, 라우트 핸들러 경로 상수 추가

--- constants/externalHref.ts
+++ constants/externalHref.ts
@@ -1,7 +1,9 @@
-import { PlatformType } from "@/app/api/login/[platform]/type";
+import { PlatformType } from "@/app/auth/[platform]/type";
 
-export const EXTERNAL_HREF: Record<PlatformType, string> = {
-  google: "https://accounts.google.com/o/oauth2/v2/auth",
-  kakao: "https://kauth.kakao.com/oauth/token",
-  github: "https://github.com/login/oauth/authorize",
+export const EXTERNAL_HREF: Record<Uppercase<PlatformType>, string> = {
+  GOOGLE: `https://accounts.google.com/o/oauth2/v2/auth?client_id=${process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID}&response_type=code&redirect_uri=${process.env.NEXT_PUBLIC_GOOGLE_REDIRECT_URI}&scope=${process.env.NEXT_PUBLIC_GOOGLE_SCOPE}`,
+  KAKAO: `https://kauth.kakao.com/oauth/authorize?client_id=${process.env.NEXT_PUBLIC_KAKAO_CLIENT_ID}&redirect_uri=${process.env.NEXT_PUBLIC_KAKAO_REDIRECT_URI}`,
+  GITHUB: `https://github.com/login/oauth/authorize?client_id=${process.env.NEXT_PUBLIC_GITHUB_CLIENT_ID}&response_type=code&redirect_uri=${process.env.NEXT_PUBLIC_GITHUB_REDIRECT_URI}`,
 };
+
+// "https://kauth.kakao.com/oauth/token" 토큰 받는 url

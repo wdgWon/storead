3151bde8caaea6c32ccb44d368682036bbbf4529
genTe <128919388+wdgWon@users.noreply.github.com>
2024-05-02 22:42:14 +0900

    [Refactoring ⚙️] 푸터 최적화

--- components/footer/footer.tsx
+++ components/footer/footer.tsx
@@ -1,19 +1,6 @@
-"use client";
-
-import { useTheme } from "next-themes";
-
-import { cn } from "@/lib/utils";
-
 function Footer() {
-  const { theme } = useTheme();
-
   return (
-    <div
-      className={cn(
-        "footer-h flex justify-center items-center shadow-md",
-        theme === "light" && "bg-gray-300",
-      )}
-    >
+    <div className="footer-h flex justify-center items-center shadow-md bg-gray-300 dark:bg-black">
       {/* FIXME: 끝나는 날짜 포함시키기 */}
       Copyright © 2024.4 ~ - All right reserved by Team Jak Dang Moi
     </div>

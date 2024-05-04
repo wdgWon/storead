09e9053770cbb186ef283137323a14ef0d5845b2
genTe <128919388+wdgWon@users.noreply.github.com>
2024-05-04 19:32:47 +0900

    [Refactoring ⚙️] tanstack query 서버 컴포넌트에서 프리패칭 될 수 있도록 최적화

--- providers/ReactQueryProvider.tsx
+++ providers/ReactQueryProvider.tsx
@@ -1,29 +1,41 @@
 "use client";
 
-import { PropsWithChildren, useState } from "react";
+import { PropsWithChildren } from "react";
 
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
-function ReactQueryProviders({ children }: PropsWithChildren) {
-  const [client] = useState(
-    new QueryClient({
-      defaultOptions: {
-        queries: {
-          refetchOnWindowFocus: false,
-          refetchOnMount: false,
-          retry: 1,
-        },
+function makeQueryClient() {
+  return new QueryClient({
+    defaultOptions: {
+      queries: {
+        refetchOnWindowFocus: false,
+        refetchOnMount: false,
+        retry: 1,
+        staleTime: 60 * 1000,
       },
-    }),
-  );
+    },
+  });
+}
+
+let browserQueryClient: QueryClient | undefined = undefined;
+
+function getQueryClient() {
+  if (typeof window === "undefined") {
+    return makeQueryClient();
+  } else {
+    if (!browserQueryClient) browserQueryClient = makeQueryClient();
+    return browserQueryClient;
+  }
+}
+
+export default function ReactQueryProviders({ children }: PropsWithChildren) {
+  const queryClient = getQueryClient();
 
   return (
-    <QueryClientProvider client={client}>
+    <QueryClientProvider client={queryClient}>
       {children}
       <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   );
 }
-
-export default ReactQueryProviders;
